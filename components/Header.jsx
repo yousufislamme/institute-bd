1fbd7f3b38fa2cce0c88170fe46ddb751d8d8e4b
saadafahmed45 <hafijnahid45@.com>
2024-04-19 02:19:24 +0600

    added the add result page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,42 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Dashboard from "./../app/dashboard/page";
 const Header = () => {
-   return (
-     <>
-     <header className='  border-b-2 px-1 md:px-5'>
-        <div className=' font-semibold flex items-center'>
-           <div>
-              <Link className=' text-xl' href="/">Institute</Link>
-           </div>
-           {/* navigation */}
-            <div className="menu w-full flex items-center justify-center gap-1">
-              <Link className=' py-2 px-1' href="/students">Students</Link>
-              <Link className=' py-2 px-1' href="/result">result</Link>
-              <Link className=' py-2 px-1' href="/blog">Blog</Link>
-              <Link className=' py-2 px-1' href="/about">About</Link>
-         </div>
-           <div>
-              <Link className='' href="/">Login</Link>
-           </div>
-        </div>        
-         </header>
-
-
-         </>
-  )
-}
+  return (
+    <>
+      <header className="  border-b-2 px-1 md:px-5">
+        <div className=" font-semibold flex items-center">
+          <div>
+            <Link className=" text-xl" href="/">
+              Institute
+            </Link>
+          </div>
+          {/* navigation */}
+          <div className="menu w-full flex items-center justify-center gap-1">
+            <Link className=" py-2 px-1" href="/students">
+              Students
+            </Link>
+            <Link className=" py-2 px-1" href="/result">
+              result
+            </Link>
+            <Link className=" py-2 px-1" href="/blog">
+              Blog
+            </Link>
+            <Link className=" py-2 px-1" href="/about">
+              About
+            </Link>
+            <Link className=" py-2 px-1" href="/dashboard">
+              Dashboard
+            </Link>
+          </div>
+          <div>
+            <Link className="" href="/">
+              Login
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default Header;
