8e1b04c863be2e0ee31df3c03ba353234cd80bef
yousufislamme <online.okk@gmail.com>
2024-05-24 09:52:44 +0600

    add a statick noticboard #7

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import NoticBoard from "@/components/NoticBoard";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <NoticBoard />
     </main>
   );
 }
