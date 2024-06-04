87be3c735e354ec08da3d08a9836a65d640a0ab7
Md Yousuf <online.okk@gmail.com>
2024-06-04 23:30:18 +0600

    add context

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,43 +1,24 @@
 "use client";
 import Button from "@/components/Button";
-import { app } from "@/firebase.init";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import { useState } from "react";
+import { Context } from "@/components/Context/Context";
+import { useContext } from "react";
 
 const Login = () => {
-  const [user, setUser] = useState("");
-  const auth = getAuth(app);
-  const googleProvider = new GoogleAuthProvider();
-
-  const handleLoginGoogle = () => {
-    signInWithPopup(auth, googleProvider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-        // localStorage save the user
-        localStorage.setItem("user", JSON.stringify(user));
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
-  };
-
+  const { handleLoginGoogle, users } = useContext(Context);
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <p>
-        Name: <span className="font-bold">{user.displayName}</span>
+        Name: <span className="font-bold">{users.displayName}</span>
       </p>
       <p>
-        Email: <span className="font-bold"> {user.email}</span>
+        Email: <span className="font-bold">{users.email}</span>
       </p>
       <img
         className="h-10 w-10 rounded-full"
-        src={user.photoURL}
-        alt={user.displayName}
+        src={users.photoURL}
+        alt={users.displayName}
       />
-      {user.emailVerified == true ? null : (
+      {users.emailVerified == true ? null : (
         <Button
           onClick={handleLoginGoogle}
           BtnTitle="Login with Google"
---
+++ components/Context/Context.js
@@ -0,0 +1,41 @@
+"use client";
+import { app } from "@/firebase.init";
+import {
+   GoogleAuthProvider,
+   getAuth,
+   signInWithPopup,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+   const [users, setUsers] = useState([]);
+
+   // Ensure getAuth is called with the initialized app
+   const auth = getAuth(app);
+   const googleProvider = new GoogleAuthProvider();
+
+   const handleLoginGoogle = () => {
+      signInWithPopup(auth, googleProvider)
+         .then((result) => {
+            const user = result.user; // Ensure this is singular as result.user is a single user object
+            setUsers(user); // Update state with the user object
+            localStorage.setItem("users", JSON.stringify(user));
+         })
+         .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+         });
+   };
+
+   useEffect(() => {
+      setUsers(JSON.parse(localStorage.getItem("users")) || []);
+   }, []);
+
+   return (
+      <Context.Provider value={{ handleLoginGoogle, users }}>
+         {children}
+      </Context.Provider>
+   );
+};
