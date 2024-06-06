006b83d7f182b8a9b9418bcc1be7a64d6abb4551
Md Yousuf <online.okk@gmail.com>
2024-06-07 00:53:11 +0600

    chnage dashbord

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -2,20 +2,18 @@ import Link from "next/link";
 
 const SideBar = () => {
   return (
-    <div> 
+    <div>
       <nav className="h-screen w-52 bg-slate-400 p-2">
-        <ul className="list-none flex flex-col gap-2 text-xl  p-4">
+        <ul className="flex list-none flex-col gap-2 p-4 text-xl">
           <li>
-             <Link href={"/dashboard/addStudent"}> Add student</Link>
+            <Link href={"/dashboard/addStudent"}> Add student</Link>
           </li>
-          <li> 
+          <li>
             <Link href={"/dashboard/addResult"}> add Result</Link>
           </li>
-          <li> 
-            <Link href={"/dashboard/addStudent"}> Manage student</Link>
-          </li>
-          <li> 
-            <Link href={"/dashboard/studentsList"}> Students List</Link>  
+
+          <li>
+            <Link href={"/dashboard/studentsList"}> Students List</Link>
           </li>
         </ul>
       </nav>
