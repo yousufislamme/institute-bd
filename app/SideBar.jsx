45f99083e592e8f139311310147a3a2e75118227
yousufislamme <online.okk@gmail.com>
2024-06-14 10:24:47 +0600

    #40 notices option and #43 add icon

--- app/SideBar.jsx
+++ app/SideBar.jsx
@@ -1,52 +1,47 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import { CiStickyNote, CiViewList } from "react-icons/ci";
+import { FaRegAddressCard } from "react-icons/fa";
 const SideBar = () => {
   const pathname = usePathname();
 
   return (
     <div>
-      <nav className="h-screen w-52 bg-slate-400 p-2">
-        <ul className="text-md flex list-none flex-col gap-5 p-4">
+      <nav className="h-screen w-52 bg-slate-200 p-2">
+        <ul className="text-md flex list-none flex-col gap-4">
           <li>
             <Link
               href="/dashboard/addStudent"
-              className={`rounded-md px-3 py-2 hover:bg-slate-500 ${
-                pathname === "/dashboard/addStudent" ? "bg-slate-500" : ""
+              className={`flex items-center gap-2 rounded-md px-3 py-2 hover:bg-slate-500 ${
+                pathname === "/dashboard/addStudent"
+                  ? "bg-slate-500 text-white"
+                  : ""
               }`}
             >
+              <FaRegAddressCard />
               Add student
             </Link>
           </li>
           <li>
             <Link
-              href="/dashboard/addResult"
-              className={`rounded-md px-3 py-2 hover:bg-slate-500 ${
-                pathname === "/dashboard/addResult" ? "bg-slate-500" : ""
-              }`}
-            >
-              Add Result
-            </Link>
-          </li>
-          <li>
-            <Link
               href="/dashboard/studentsList"
-              className={`rounded-md px-3 py-2 hover:bg-slate-500 ${
+              className={`flex items-center gap-2 rounded-md px-3 py-2 hover:bg-slate-500 ${
                 pathname === "/dashboard/studentsList" ? "bg-slate-500" : ""
               }`}
             >
+              <CiViewList />
               Students List
             </Link>
           </li>
           <li>
             <Link
-              href="/dashboard/notice"
-              className={`rounded-md px-3 py-2 hover:bg-slate-500 ${
-                pathname === "/dashboard/notice" ? "bg-slate-500" : ""
+              href="/dashboard/notices"
+              className={`flex items-center gap-2 rounded-md px-3 py-2 hover:bg-slate-500 ${
+                pathname === "/dashboard/notices" ? "bg-slate-500" : ""
               }`}
             >
-              Notice
+              <CiStickyNote /> Notices
             </Link>
           </li>
         </ul>
