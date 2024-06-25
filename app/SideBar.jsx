2820c9830e337549792a3e846ef11f20e29f1665
yousufislamme <online.okk@gmail.com>
2024-06-25 07:23:38 +0600

    add for add teacher

--- app/SideBar.jsx
+++ app/SideBar.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CiEdit, CiStickyNote, CiViewList } from "react-icons/ci";
 import { FaRegAddressCard } from "react-icons/fa";
+import { IoIosPersonAdd } from "react-icons/io";
+import { TbArrowsRandom } from "react-icons/tb";
 
 const SideBar = () => {
   const pathname = usePathname();
@@ -57,11 +59,23 @@ const SideBar = () => {
           </li>
           <li>
             <Link
+              href="/dashboard/addTeachers"
+              className={`flex items-center gap-2 rounded-md px-3 py-2 hover:bg-slate-500 ${
+                pathname === "/dashboard/addTeachers" ? "bg-slate-500" : ""
+              }`}
+            >
+              <IoIosPersonAdd />
+              Add Teacher
+            </Link>
+          </li>
+          <li>
+            <Link
               href="/dashboard/addRole"
               className={`flex items-center gap-2 rounded-md px-3 py-2 hover:bg-slate-500 ${
                 pathname === "/dashboard/addRole" ? "bg-slate-500" : ""
               }`}
             >
+              <TbArrowsRandom />
               Add Role
             </Link>
           </li>
---
+++ app/dashboard/addTeachers/page.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+
+const Teachers = () => {
+  const [teacherData, setTeacherData] = useState({
+    teacherName: "",
+    teacherBody: "",
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTeacherData({
+      ...teacherData,
+      [name]: value,
+    });
+  };
+
+  const handleTeacherAdd = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/teachers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(teacherData),
+    });
+  };
+  return (
+    <div className="p-5">
+      <h1>Teachers</h1>
+      <form className="" onSubmit={handleTeacherAdd}>
+        <div className="mt-10 flex w-[600px] flex-col gap-2">
+          <input
+            onChange={handleInputChange}
+            className="w-full rounded-lg border-2 p-2 font-bold outline-none"
+            placeholder="Title"
+            name="teacherName"
+            value={teacherData.teacherName}
+          />
+          <textarea
+            onChange={handleInputChange}
+            name="teacherBody"
+            value={teacherData.teacherBody}
+            className="h-52 rounded-lg border-2 p-2 outline-none"
+            placeholder="Blog Body"
+          ></textarea>
+        </div>
+        <button
+          className="mt-2 rounded-lg bg-green-500 px-5 py-2"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Teachers;
